1ae6666d8e13af22f2262e857d7a33679d1e15d5
anjanidwilestari <ayianjani774@gmail.com>
2024-12-23 08:40:01 +0700

    books-create bisa || books/tambah gabisa || kenapa?

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,8 @@ use App\Http\Controllers\BookController;
 
 
 
-Route::get('/books', [BookController::class, 'index'])->name('books.index');
-Route::get('/books/{id}', [BookController::class, 'show'])->name('books.show');
-Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
-Route::post('/books', [BookController::class, 'store'])->name('books.store');
-Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
-Route::post('/books/{book}', [BookController::class, 'update'])->name('books.update');
-Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
+
+
 
 
 
@@ -44,9 +39,21 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    //Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //Buku
+    Route::get('/books', [BookController::class, 'index'])->name('books.index');
+    Route::get('/books/{id}', [BookController::class, 'show'])->name('books.show');
+    Route::get('/books-create', [BookController::class, 'create'])->name('books.create');
+    Route::post('/books', [BookController::class, 'store'])->name('books.store');
+    Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
+    Route::post('/books/{book}', [BookController::class, 'update'])->name('books.update');
+    Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
+
+
 });
 
 require __DIR__.'/auth.php';
